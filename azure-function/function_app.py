fb4bacac684ac31b0dc17ee9efa71a92bd7d2717
Thanh Tran <thanh.tran@wiz.io>
2025-03-05 19:26:44 -0800

    initial commit

---
+++ azure-function/function_app.py
@@ -0,0 +1,27 @@
+import azure.functions as func
+import logging
+
+secret = "abc123asdf3456"
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+
+@app.route(route="thanh_http_trigger_azure_func")
+def thanh_http_trigger_azure_func(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. The secr is {secret}.  This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
